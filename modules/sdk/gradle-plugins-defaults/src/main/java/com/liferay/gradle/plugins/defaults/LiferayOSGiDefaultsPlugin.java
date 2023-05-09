bacd46c2510aa46e739e798c0b26745ca9e0a074
Peter Shin <peter.shin@liferay.com>
2023-05-09 10:41:22 -0500

    LPS-181508 Configuration api

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
@@ -2290,7 +2290,7 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 
 		if (projectPath.startsWith(":dxp:apps:osb:")) {
 			_configureDependenciesReleaseAPI(
-				project, JavaPlugin.COMPILE_CONFIGURATION_NAME);
+				project, JavaPlugin.API_CONFIGURATION_NAME);
 			_configureDependenciesReleaseAPI(
 				project, JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME);
 			_configureDependenciesReleaseAPI(
@@ -2305,13 +2305,13 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 			projectPath.startsWith(":private:core:")) {
 
 			_configureConfigurationTransitive(
-				project, JavaPlugin.COMPILE_CONFIGURATION_NAME, false);
+				project, JavaPlugin.API_CONFIGURATION_NAME, false);
 			_configureConfigurationTransitive(
 				project, JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME,
 				false);
 
 			_configureDependenciesGroupPortal(
-				project, appBndFile, JavaPlugin.COMPILE_CONFIGURATION_NAME,
+				project, appBndFile, JavaPlugin.API_CONFIGURATION_NAME,
 				publishing);
 			_configureDependenciesGroupPortal(
 				project, appBndFile,
