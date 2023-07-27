c0c1c84762cf0699cc082bff2ad1e00a26f1a38d
Peter Shin <peter.shin@liferay.com>
2023-07-27 15:44:09 -0500

    LPS-181508 Test

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayDefaultsPlugin.java
@@ -42,10 +42,14 @@ public class LiferayDefaultsPlugin extends LiferayPlugin {
 		MavenPublishDefaultsPlugin.INSTANCE.apply(project);
 		NodeDefaultsPlugin.INSTANCE.apply(project);
 
-		File relengDir = LiferayRelengUtil.getRelengDir(project);
+		String buildProfile = System.getProperty("build.profile");
 
-		if (relengDir != null) {
-			LiferayRelengPlugin.INSTANCE.apply(project);
+		if (Validator.isNull(buildProfile)) {
+			File relengDir = LiferayRelengUtil.getRelengDir(project);
+
+			if (relengDir != null) {
+				LiferayRelengPlugin.INSTANCE.apply(project);
+			}
 		}
 
 		String projectPath = project.getPath();
